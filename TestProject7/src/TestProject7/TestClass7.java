aad77f23f7109c9b7363577561941edb056d65ab
User <User@Mylap>
2022-07-20 13:42:42 +0530

    Push the code

--- TestProject7/src/TestProject7/TestClass7.java
+++ TestProject7/src/TestProject7/TestClass7.java
@@ -5,6 +5,7 @@ public class TestClass7 {
 	public static void main(String[] args) {
 	System.out.println("Hi");
 	System.out.println("Welcome");
+	System.out.println("Sucessfully push the code on git repo");
 
 	}
 
